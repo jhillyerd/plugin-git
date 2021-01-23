626aa5fa7694d24f5aa9160f74b82f243c149697
James Hillyerd <james@hillyerd.com>
2021-01-23 10:35:11 -0800

    gbda: protect main in addition to master branch

--- functions/gbda.fish
+++ functions/gbda.fish
@@ -1,5 +1,5 @@
 function gbda -d "Delete all branches merged in current HEAD"
   git branch --merged | \
-    command grep -vE  '^\*|^\s*(master|develop)\s*$' | \
+    command grep -vE  '^\*|^\s*(master|main|develop)\s*$' | \
     command xargs -n 1 git branch -d
 end
