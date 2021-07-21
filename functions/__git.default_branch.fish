033683ead175c6b458dbeea94510214b62f11625
Tjebbe Vlieg <tvlieg@bol.com>
2021-07-21 17:11:01 +0200

    feat: use git init.defaultBranch if set

--- functions/__git.default_branch.fish
+++ functions/__git.default_branch.fish
@@ -1,6 +1,7 @@
 function __git.default_branch -d "Fallback to main when master branch is not found"
   # Silently fail when not in git directory
   command git rev-parse --git-dir &>/dev/null; or return
+  command git config --get init.defaultBranch; and return
   if command git show-ref -q --verify refs/heads/master
     echo master
   else
