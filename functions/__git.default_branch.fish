4d44a151503a51a42e7adad7e99ac45c683e0866
tvlieg <tjebbe@vlieg.me>
2021-07-22 17:29:42 +0200

    feat: use git init.defaultBranch if set and present (#61)
    * feat: use git init.defaultBranch if set
    * fix: check for existence of defaultBranch
    
    Co-authored-by: Tjebbe Vlieg <tvlieg@bol.com>

--- functions/__git.default_branch.fish
+++ functions/__git.default_branch.fish
@@ -1,7 +1,10 @@
 function __git.default_branch -d "Fallback to main when master branch is not found"
   # Silently fail when not in git directory
   command git rev-parse --git-dir &>/dev/null; or return
-  if command git show-ref -q --verify refs/heads/master
+  set -l defaultBranch (command git config --get init.defaultBranch)
+  if command git show-ref -q --verify refs/heads/{$defaultBranch}
+    echo $defaultBranch
+  else if command git show-ref -q --verify refs/heads/master
     echo master
   else
     echo main
