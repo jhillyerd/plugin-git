e1bba44dcfeb9449dcc718e9ced6b9f84dfb8723
okuuva <okuuva@users.noreply.github.com>
2021-09-05 10:03:17 +0300

    Update __git.default_branch function description

--- functions/__git.default_branch.fish
+++ functions/__git.default_branch.fish
@@ -1,4 +1,4 @@
-function __git.default_branch -d "Fallback to main when master branch is not found"
+function __git.default_branch -d "Use init.defaultBranch if it's set and exists, otherwise use main if it exists. Falls back to master"
   command git rev-parse --git-dir &>/dev/null; or return
   if set -l default_branch (command git config --get init.defaultBranch)
     and command git show-ref -q --verify refs/heads/{$default_branch}
