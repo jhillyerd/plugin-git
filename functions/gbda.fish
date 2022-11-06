955b02c400c1ee9f7de0da683d2b315244cd0d80
James Roeder <jmaroeder@gmail.com>
2022-11-06 17:52:36 -0500

    Delete squashed branches in gbda (#87)

--- functions/gbda.fish
+++ functions/gbda.fish
@@ -1,5 +1,14 @@
-function gbda -d "Delete all branches merged in current HEAD"
+function gbda -d "Delete all branches merged in current HEAD, including squashed"
   git branch --merged | \
     command grep -vE  '^\*|^\s*(master|main|develop)\s*$' | \
     command xargs -n 1 git branch -d
+
+  set -l default_branch (__git.default_branch)
+  git for-each-ref refs/heads/ "--format=%(refname:short)" | \
+    while read branch
+      set -l merge_base (git merge-base $default_branch $branch)
+      if string match -q -- '-*' (git cherry $default_branch (git commit-tree (git rev-parse $branch\^{tree}) -p $merge_base -m _))
+        git branch -D $branch
+      end
+    end
 end
