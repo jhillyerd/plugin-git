d713f078d9e02db78ae3d315a135b13eb4dfff4e
James Hillyerd <james@hillyerd.com>
2023-02-11 11:41:24 -0800

    Prevent omf from running fisher init

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -2,6 +2,8 @@
 # This can be removed when we cleanup other universal abbr code.
 functions -e _git_install _git_update _git_uninstall
 
-# fisher initialization.
-source $__fish_config_dir/functions/__git.init.fish
-__git.init
+# fisher initialization, protected as omf also tries to run it.
+if test -f $__fish_config_dir/functions/__git.init.fish
+  source $__fish_config_dir/functions/__git.init.fish
+  __git.init
+end
