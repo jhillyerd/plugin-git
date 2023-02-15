0bdaea464232a64a5ab2c5dd1fbe8eee39c195a7
James Hillyerd <james@hillyerd.com>
2023-02-15 13:27:31 -0800

    Quiet reset output
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- functions/gtest.fish
+++ functions/gtest.fish
@@ -6,14 +6,14 @@ function gtest -d "test command on staged changes only"
   # Stash working dir, keeping index changes.
   git stash push -q --keep-index --include-untracked; or return
 
-  # Run test command and cleanup any trackable output.
+  # Run test command against index changes only.
   command $argv
   set cmdstatus $status
 
   # Return working dir and index to original state.
   # Note: reset + restore is required to prevent merge conflicts
   # when popping the stash.
-  git reset
+  git reset -q
   git restore .
   git stash pop -q --index; or return $status
 
