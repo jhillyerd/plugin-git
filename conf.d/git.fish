74450ffecf41a0dc4862143739840d0df52a5393
zhaolei <2290938350@qq.com>
2023-02-20 00:20:48 +0800

    Load configuration via fisher_path (#95)

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -3,7 +3,8 @@
 functions -e _git_install _git_update _git_uninstall
 
 # fisher initialization, protected as omf also tries to run it.
-if test -f $__fish_config_dir/functions/__git.init.fish
-  source $__fish_config_dir/functions/__git.init.fish
+set -q fisher_path; or set -l fisher_path $__fish_config_dir
+if test -f $fisher_path/functions/__git.init.fish
+  source $fisher_path/functions/__git.init.fish
   __git.init
 end
