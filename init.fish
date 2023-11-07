c2b38f53f0b04bc67f9a0fa3d583bafb3f558718
hexclover <47456195+hexclover@users.noreply.github.com>
2023-11-08 02:12:47 +0800

    Check for presence of `$path/functions/__git.init.fish` before sourcing it (#103)
    For Nix home-manager

--- init.fish
+++ init.fish
@@ -1,3 +1,9 @@
 # omf initialization.
-source $path/functions/__git.init.fish
+# $path is only defined for oh-my-fish. home-manager activates this plugin by
+# adding the full path of functions/ to fish_function_path, and then sourcing
+# init.fish, so let's skip sourcing __git.init.fish before calling __git.init.
+set -l __git_init_path "$path/functions/__git.init.fish"
+if [ -f "$__git_init_path" ];
+    source "$__git_init_path"
+end
 __git.init
