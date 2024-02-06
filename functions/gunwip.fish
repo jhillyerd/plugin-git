6336017c16c02b1e9e708dd4eb233e66a18b62fe
James Hillyerd <james@hillyerd.com>
2024-02-06 15:42:59 -0800

    gunwip: remove extraneous backslashes (#105)
    Signed-off-by: James Hillyerd <james@hillyerd.com>

--- functions/gunwip.fish
+++ functions/gunwip.fish
@@ -3,5 +3,5 @@
 # When you want to go back to work, just unwip it
 #
 function gunwip -d "git uncommit the work-in-progress branch"
-  git log -n 1 | grep -q -c "\-\-wip\-\-"; and git reset HEAD~1
+  git log -n 1 | grep -q -c "\--wip--"; and git reset HEAD~1
 end
